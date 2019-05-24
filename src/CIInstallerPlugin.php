806787ee96ee873a7a5b7d04c39498b5325e6298
Cameron Green <cam@uq.edu.au>
2019-05-24 11:33:24 +1000

    Implementing interface

--- src/CIInstallerPlugin.php
+++ src/CIInstallerPlugin.php
@@ -6,7 +6,7 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class CIInstallerPlugin {
+class CIInstallerPlugin implements PluginInterface {
   public function activate(Composer $composer, IOInterface $io) {
     $installer = new CIInstaller($io, $composer);
     $composer->getInstallationManager()->addInstaller($installer);
